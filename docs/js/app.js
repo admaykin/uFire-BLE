ba62ca92b0c841b3e03a165859887148714ce8f0
Justin Decker <justin@ufire.co>
2019-07-30 09:56:51 -0700

    removed install alert

--- docs/js/app.js
+++ docs/js/app.js
@@ -44,15 +44,6 @@ var app = new Vue({
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('service-worker.js');
     }
-
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    //e.preventDefault();
-    // Stash the event so it can be triggered later.
-    this.deferredPrompt = e;
-    alert("before install");
-  });
-
   },
   methods: {
     connect,
